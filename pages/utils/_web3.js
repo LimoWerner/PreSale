6dac562369b52026c54255a6dad8548b6b5274e7
Limo Werner <103921158+ralresearcher@users.noreply.github.com>
2022-05-05 17:06:38 +0530

    Update _web3.js

--- pages/utils/_web3.js
+++ pages/utils/_web3.js
@@ -32,7 +32,7 @@ export const mintGift = async (account, numberOfTokens, proof) => {
   console.log('minting gift...');
   const amount = (numberOfTokens * 0).toString();
     const amountToWei = web3.utils.toWei(amount, 'ether');
-  const result = sampleNFT.methods.mintGift(proof,numberOfTokens).send({ from: account,value: amountToWei }).then((result) => {
+  const result = sampleNFT.methods.mintGift(numberOfTokens,proof).send({ from: account,value: amountToWei }).then((result) => {
       return {
         success: true,
         status: `✅ Check out your transaction on Etherscan: https://etherscan.io/tx/` + result
